758fb87ef24491ac25910460825b92d21fc02c3e
WojciechMankowski <wojtekm510@gmail.com>
2021-09-29 11:25:13 +0200

    add url in nav

--- collection_of_English_learning_materials/wsgi.py
+++ collection_of_English_learning_materials/wsgi.py
@@ -1,19 +1,16 @@
-"""
-WSGI config for collection_of_English_learning_materials project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
-import os
-
+# +++++++++++ DJANGO +++++++++++
+# Aby użyć własnej aplikacji Django, użyj kodu w następujący sposób:
+import  os
+import  sys
+path = '/home/wojtek92/collection_of_English_learning_materials'
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collection_of_English_learning_materials.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collection_of_English_learning_materials.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+# from whitenoise.django import DjangoWhiteNoise
+# application = DjangoWhiteNoise(application)
